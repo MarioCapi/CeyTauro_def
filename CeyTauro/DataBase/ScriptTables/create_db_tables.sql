1116a0f6d2c75c1eeff3598571dd95d1ea84c563
Mario Barragan <mariocapi90mym@gmail.com>
2024-11-14 17:29:08 -0500

    se crea tabla, sp y apis de la entidad productos

--- CeyTauro/DataBase/ScriptTables/create_db_tables.sql
+++ CeyTauro/DataBase/ScriptTables/create_db_tables.sql
@@ -10,3 +10,11 @@ CREATE TABLE "Management".usuarios (
     password VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE "Management".productos 
+(id_producto SERIAL PRIMARY KEY,
+codeProducto INT NOT NULL UNIQUE,
+nombre_producto VARCHAR(255) NOT NULL,
+descripcion TEXT, unidad_medida VARCHAR(50),
+precio_unitario DECIMAL(10, 2));
+
