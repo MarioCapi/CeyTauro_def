7bab1dae9598c1fe6902cc15e3f4d1060c6ab8e0
Mario Barragan <mariocapi90mym@gmail.com>
2024-11-19 17:01:13 -0500

    se cambia logica del api leer usuarios funcionando

--- CeyTauro/DataBase/TestSP/Test_SP_Users.sql
+++ CeyTauro/DataBase/TestSP/Test_SP_Users.sql
@@ -1,7 +1,7 @@
 -- test para crear usuario 
 DO $$
 BEGIN
-    CALL "Management".sp_create_usuario('mario', '123456', 'correo@ejemplo.com');
+    CALL "management".sp_create_usuario('mario', '123456', 'correo@ejemplo.com');
 END $$;
 
 
@@ -12,17 +12,23 @@ DECLARE
 BEGIN
     -- Prueba 1: Leer un usuario específico que existe
     RAISE NOTICE 'Prueba 1: Leer usuario con ID 1';
-    CALL "Management".sp_read_usuario(resultado, 1);
+    CALL "management".sp_read_usuario(1, resultado);
     RAISE NOTICE 'Resultado: %', resultado;
 
     -- Prueba 2: Leer un usuario específico que no existe
-    RAISE NOTICE 'Prueba 2: Leer usuario con ID inexistente (ID 99999)';
-    CALL "Management".sp_read_usuario(resultado, 99999);
+    RAISE NOTICE 'Prueba 2: Leer usuario con ID inexistente (99999)';
+    CALL "management".sp_read_usuario(99999, resultado);
     RAISE NOTICE 'Resultado: %', resultado;
 
     -- Prueba 3: Leer todos los usuarios
     RAISE NOTICE 'Prueba 3: Leer todos los usuarios';
-    CALL "Management".sp_read_usuario(resultado, NULL);
+    CALL "management".sp_read_usuario(999999999, resultado);
+    RAISE NOTICE 'Resultado: %', resultado;
+
+    -- Prueba 4: Tabla sin registros (asegúrate de probar esto con la tabla vacía)
+    RAISE NOTICE 'Prueba 4: Leer todos los usuarios en una tabla vacía';
+    CALL "management".sp_read_usuario(999999999, resultado);
     RAISE NOTICE 'Resultado: %', resultado;
 END;
 $$;
+
