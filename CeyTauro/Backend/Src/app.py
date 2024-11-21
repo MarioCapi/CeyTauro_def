f052d0d863c66034b17caae78f64cff561233aa9
jairodiaz007 <jairodiaz007@prueba.co>
2024-11-21 15:31:32 -0500

    se agrega CRUD con FRONT de ventas

--- CeyTauro/Backend/Src/app.py
+++ CeyTauro/Backend/Src/app.py
@@ -5,7 +5,7 @@ from blueprints.usuarios import usuarios_bp
 from blueprints.Products import products_bp
 from blueprints.Provider import provider_bp
 from blueprints.inventory import invetory_bp
-##from blueprints.ventas import ventas_bp
+from blueprints.Client import client_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -14,7 +14,8 @@ app.register_blueprint(usuarios_bp, url_prefix='/api/usuarios')
 app.register_blueprint(products_bp, url_prefix='/api/products')
 app.register_blueprint(provider_bp, url_prefix='/api/providers')
 app.register_blueprint(invetory_bp, url_prefix='/api/inventory')
-##app.register_blueprint(ventas_bp, url_prefix='/api/ventas')
+app.register_blueprint(client_bp, url_prefix='/api/client')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
