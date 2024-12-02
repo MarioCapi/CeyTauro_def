27125cf477945765d843135239cf6f8763681b43
jairodiaz007 <jairodiaz007@prueba.co>
2024-12-02 13:29:37 -0500

    ajustes del API para productos

--- CeyTauro/Backend/Src/blueprints/Products.py
+++ CeyTauro/Backend/Src/blueprints/Products.py
@@ -9,20 +9,20 @@ products_bp = Blueprint('products', __name__)
 @cross_origin
 @products_bp.route('/api/products_create', methods=['POST'])
 def create_product():
+    data = request.json
     try:
-        data = request.json    
-        result = execute_procedure_read('sp_create_producto', (data['codeProducto'],data['nombre_producto'], 
-                                                                data['unidad_de_medida'], data['descripcion'],data['precio_unitario']))
-        return jsonify({'message': result}), 200
+        result = execute_procedure_read('sp_insert_product', (data['codeProducto'],data['nombre_producto'],
+                                                            data['unidad_de_medida'], data['descripcion'],data['precio_unitario']))
+        return jsonify({'message':result}),200
     except Exception as e:
-            return jsonify({"error": str(e)}), 500
+        return jsonify({'message': 'Error creando producto','error':str(e)}),500
 
 
 @cross_origin
-@products_bp.route('/api/products_read/<int:id>', methods=['GET'])
-def read_producto(id):
+@products_bp.route('/api/products_read', methods=['GET'])
+def read_producto():
     try:
-        result = execute_procedure_read('sp_read_producto', (id,))
+        result = execute_procedure_read('sp_read_producto')
         return jsonify({"message":result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
@@ -32,20 +32,24 @@ def read_producto(id):
 @products_bp.route('/api/products_update', methods=['PUT'])
 def products_update():
     data = request.json    
-    try:
-        execute_procedure('sp_update_producto', (int(data['id']), str(data['nombre']), str(data['unidad']), int(data['precio'])))
-        return jsonify({'message': 'Producto actualizado exitosamente'})
+    try:        
+        result = execute_procedure_read('sp_update_product', (data['codeProducto'],
+                                                            data['nombre_producto'],
+                                                            data['unidad_de_medida'], 
+                                                            data['descripcion'],
+                                                            data['precio_unitario']))
+        return jsonify({'message':result}),200
     except Exception as e:
         return jsonify({'error': 'Error actualizando el producto'}), 500
 
 @cross_origin
-@products_bp.route('/api/products_delete/<int:id>', methods=['DELETE'])
-def delete_producto(id):
+@products_bp.route('/api/products_delete/<id>', methods=['DELETE'])
+def delete_producto(id):    
     try:
-        execute_procedure('sp_delete_producto', (id,))
-        return jsonify({'message': "Producto Eliminado"})
+        result = execute_procedure_read('sp_delete_producto', (id,))
+        return jsonify({'message':result}),200
     except Exception as e:
-        return jsonify({'message': "Error Eliminando Producto Eliminado"}), 500
+        return jsonify({'error': 'Error eliminando el producto'}), 500
     
 
 #if __name__ == '__main__':
--- CeyTauro/DataBase/TestSP/test_SP_Products.sql
+++ CeyTauro/DataBase/TestSP/test_SP_Products.sql
@@ -6,6 +6,16 @@ BEGIN
     RAISE NOTICE 'Resultado: %', v_result;
 END $$;
 
+--test OK--
+URL:  http://127.0.0.1:5000/api/products/api/products_create;
+{
+    "codeProducto": "007",
+    "nombre_producto": "Producto de Prueba desde postman",
+    "unidad_de_medida": "kg",
+    "descripcion": "Este es un producto de prueba para el endpoint desde postman.",
+    "precio_unitario": 157.99
+}
+
 
 --Test for Users SPs
 
@@ -16,6 +26,49 @@ BEGIN
     CALL management.sp_read_producto(v_result);    
     RAISE NOTICE 'Resultado: %', v_result;
 END $$;
+
+--test OK--
+GET: http://127.0.0.1:5000/api/products/api/products_read
+
+{
+    "message": [
+        {
+            "codeProducto": "001",
+            "descripcion": "generic_prod",
+            "id_producto": 1,
+            "nombre_producto": "nomProd",
+            "precio_unitario": 8900.0,
+            "unidad_de_medida": "kg"
+        },
+        {
+            "codeProducto": "002",
+            "descripcion": "generic_prod",
+            "id_producto": 3,
+            "nombre_producto": "nomProd",
+            "precio_unitario": 8900.0,
+            "unidad_de_medida": "kg"
+        },
+        {
+            "codeProducto": "003",
+            "descripcion": "generic_prod",
+            "id_producto": 5,
+            "nombre_producto": "nomProd",
+            "precio_unitario": 8900.0,
+            "unidad_de_medida": "kg"
+        },
+        {
+            "codeProducto": "007",
+            "descripcion": "Este es un producto de ACTUALIZADO para el endpoint desde postman.",
+            "id_producto": 6,
+            "nombre_producto": "Producto de Prueba ACTUALIZADO DESDE EL postman",
+            "precio_unitario": 15.8,
+            "unidad_de_medida": "kg"
+        }
+    ]
+}
+
+
+
 
 
 
@@ -28,6 +81,9 @@ BEGIN
     CALL management.sp_delete_producto(v_id,v_result);    
     RAISE NOTICE 'Resultado: %', v_result;
 END $$;
+--test ok-- 
+DELETE: http://127.0.0.1:5000/api/products/api/products_delete/008
+
 
 
 
@@ -44,4 +100,13 @@ BEGIN
     RAISE NOTICE 'Resultado: %', v_result;
 END $$;
 
+--test OK--
+put:  http://127.0.0.1:5000/api/products/api/products_update
+{
+    "codeProducto": "007",
+    "nombre_producto": "Producto de Prueba ACTUALIZADO DESDE EL postman",
+    "unidad_de_medida": "kg",
+    "descripcion": "Este es un producto de ACTUALIZADO para el endpoint desde postman.",
+    "precio_unitario": 15.8
+}
 
